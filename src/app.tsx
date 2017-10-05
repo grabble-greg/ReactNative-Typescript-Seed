9a4b0705ed0ff0cc040a0ca3bfc2f3a9741869b2
Joey Ciechanowicz <joey.ciechanowicz@macmillan.com>
2017-10-05 22:08:22 +0100

    Moved to typescript

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
+
+import * as React from 'react';
 import {
   Platform,
   StyleSheet,
@@ -19,7 +15,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends React.Component<{}> {
   render() {
     return (
       <View style={styles.container}>
